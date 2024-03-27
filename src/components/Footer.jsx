d668cb7ada9897fd6d68ffc5e6238bc6d5ee1d1f
LUFEC4T0 <luigi02190622@gmail.com>
2024-03-27 14:07:29 -0500

    Se sube proyecto base y footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+        <img className='w-[100%] h-[1.5rem]' src="../public/barraFooter.png" alt="" />
+            <footer className="bg-zinc-800 text-white text-center p-5 h-[20rem] flex flex-wrap justify-around">
+                <div className='flex flex-col w-[18rem] justify-between p-3'>
+                    <img className='w-[5rem]' src="../public/Acuarios HLM logo blanco (sin letras) PNG.png" alt="" />
+                    <p className='mt-3 text-justify text-[.9rem] text-[#c9c9c9]' >Lorem ipsum dolor sit, amet consectetur adipisicing elit rvrcvr recerc er cercer cercerc cercerc cercertvrtbrtyb.</p>
+                    <div className='flex gap-5 items-start'>
+                        <img className='w-[1.3rem] mt-3' src="../public/logoFacebook.png" alt="" />
+                        <img className='w-[1.3rem] mt-3' src="../public/logoInstagram.png" alt="" />
+                        <img className='w-[1.3rem] mt-3' src="../public/logoWhatsapp.png" alt="" />
+                    </div>
+                    <p className='mt-3 text-[.7rem] text-[#c9c9c9] text-start'>&copy; 2022 Acuario Y Peces HLM. All rights reserved.</p>
+                </div>
+                <div className='flex flex-col w-[18rem] justify-center gap-3 p-3'>
+                    <p className='text-start'>Paginas</p>
+                    <div>
+                        <p className='my-2 text-[#c9c9c9] text-[.9rem] text-start'>Inicio</p>
+                        <p className='my-2 text-[#c9c9c9] text-[.9rem] text-start'>Tienda</p>
+                        <p className='my-2 text-[#c9c9c9] text-[.9rem] text-left'>Sobre Nosotros</p>
+                        <p className='my-2 text-[#c9c9c9] text-[.9rem] text-start'>Contacto</p>
+                        <p className='my-2 text-[#c9c9c9] text-[.9rem] text-start'>Inicia Sesión</p>
+                    </div>
+                </div>
+                <div className='flex flex-col w-[18rem] justify-center gap-3 p-3'>
+                    <p className='text-start'>Contacto</p>
+                    <div className='flex flex-col'>
+                        <div className='flex items-center'>
+                            <img className='w-[1rem] h-[1rem] mr-2' src="../public/logoTelefono.png" alt="" /><p className='my-2 text-[#c9c9c9] text-[.9rem]'>+57 319 682 0102</p>
+                        </div>
+                        <div className='flex items-center'>
+                            <img className='w-[1.2rem] h-[1.2rem] mr-2' src="../public/logoCorreo.png" alt="" /><p className='my-2 text-[#c9c9c9] text-[.9rem]'>acuarioypeceshlm@gmail.com</p>
+                        </div>
+                        <div className='flex items-center'>
+                            <img className='w-[1.2rem] h-[1.2rem] mr-2' src="../public/logoUbicacion.png" alt="" /><p className='my-2 text-[#c9c9c9] text-[.9rem]'>Calle 55 # 1B-39 (La Dorada)</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <img className='w-[100%] h-[1.5rem]' src="../public/barraFooter.png" alt="" />
+        </>
+    )
+}
+
+export default Footer
