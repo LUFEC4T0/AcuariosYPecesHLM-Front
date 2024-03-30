11fa411451321f20580e9bd74f36cbccf064244c
LUFEC4T0 <luigi02190622@gmail.com>
2024-03-30 03:21:25 -0500

    Se realizo header y about sencillo y responsive de una vez, se modifico footer para que fuera responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 const About = () => {
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-[#A62190] via-[#6583BF] to-[#48B0D9]">
-        <h1 className='text-cyan-300 text-4xl font-bold mb-10 font-mono '>Sobre nosotros</h1>
-        <div className='flex gap-10'>
-            <img className='w-[45rem] shadow-black shadow-2xl rounded-xl' src="../public/imgAbout.jpg" alt="" />
-            <div className='w-[45rem] flex items-center'>
-                <p className='text-justify text-gray-200 font-mono'>En <span className='font-bold text-cyan-300'>Acuarios y Peces HLM</span>, nos apasiona proporcionarte las herramientas y conocimientos necesarios para explorar el maravilloso mundo de los acuarios y los peces. Ya seas un aficionado entusiasta o un profesional experimentado, nuestro objetivo es ser tu socio de confianza en cada paso del camino. Desde la selección del acuario perfecto hasta la creación de un entorno acuático próspero y equilibrado, estamos aquí para ayudarte a hacer realidad tus sueños acuáticos.<br/><br/> Con una amplia gama de productos de calidad y un equipo dedicado de expertos en acuarios, estamos comprometidos a brindarte una experiencia excepcional en el cuidado de peces y acuarios. Únete a nosotros en nuestro viaje para explorar la belleza y la tranquilidad del mundo submarino y descubre todo lo que <span className='font-bold text-cyan-300'>Acuarios y Peces HLM</span> tiene para ofrecer.</p>
+      <Header />
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-col">
+          <div className="lg:w-4/6 mx-auto">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-full w-full" src="../public/imgAbout.jpg"></img>
             </div>
+            <div className="flex flex-col sm:flex-row mt-10">
+              <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-800 text-gray-600">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <div className="flex flex-col items-center text-center justify-center">
+                  <h2 className="font-medium title-font mt-4 text-white text-lg">Luis Carrascal</h2>
+                  <div className="w-12 h-1 bg-[#48B0D9] rounded mt-2 mb-4"></div>
+                  <p className="text-base text-[#c9c9c9]">Soy el creador de este hermoso negocio, por 8 años que me he dedicado al mundo de los peces, te comparto quienes somos</p>
+                </div>
+              </div>
+              <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                <p className="leading-relaxed text-lg mb-4 text-[#c9c9c9]" >En Acuarios y Peces HLM, nos apasiona proporcionarte las herramientas y conocimientos necesarios para explorar el maravilloso mundo de los acuarios y los peces. Ya seas un aficionado entusiasta o un profesional experimentado, nuestro objetivo es ser tu socio de confianza en cada paso del camino. Desde la selección del acuario perfecto hasta la creación de un entorno acuático próspero y equilibrado, estamos aquí para ayudarte a hacer realidad tus sueños acuáticos. Con una amplia gama de productos de calidad y un equipo dedicado de expertos en acuarios, estamos comprometidos a brindarte una experiencia excepcional en el cuidado de peces y acuarios. Únete a nosotros en nuestro viaje para explorar la belleza y la tranquilidad del mundo submarino y descubre todo lo que Acuarios y Peces HLM tiene para ofrecer.</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
+      <Footer />
     </>
   )
 }
