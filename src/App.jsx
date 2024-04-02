a7f64798249839433f750a933b917e8f52d2af41
OrlandoConci <orlandomarceloconci@gmail.com>
2024-04-01 22:33:10 -0300

    Providers Employees new Provider

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import AllEmployees from './pagesSystem/AllEmployees'
 import CartsDetails from './componentsSystem/CartsDetails'
 import SalesDetails from './componentsSystem/SalesDetails'
 import ProductDetails from './pages/ProductDetails'
+import ViewAllProvider from './pagesSystem/ViewAllProvider'
 
 
 
@@ -44,6 +45,7 @@ function App() {
                   <Route path="/admin/viewAllClients/:id/:tipo" element={<CartsDetails/>}/>
                   <Route path="/admin/allEmployees" element={<AllEmployees/>}/>
                   <Route path="/admin/salesDetails/:id" element={<SalesDetails/>}/>
+                  <Route path="/admin/viewAllProviders" element={<ViewAllProvider/>}/>
                   
               </Route>
 
