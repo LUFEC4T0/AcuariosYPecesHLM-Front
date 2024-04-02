8c9327a9bce1712968bb787b77577bf723f2ed68
LUFEC4T0 <luigi02190622@gmail.com>
2024-04-01 20:50:43 -0500

    se actualizan clientinfo y shop

--- src/pages/ClientInfo.jsx
+++ src/pages/ClientInfo.jsx
@@ -30,13 +30,13 @@ const ClientInfo = () => {
         setclientData(datosEditados);
         console.log(clientData)
         setEditando(false);
-        axios.post("/api/current/editData",datosEditados,{
+        axios.post("/api/current/editData", datosEditados, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
-        }).then(response =>{
+        }).then(response => {
             alert("Data updated!")
-        }).catch(error=>{
+        }).catch(error => {
             alert.log("Something went wrong!")
         })
     };
@@ -49,28 +49,30 @@ const ClientInfo = () => {
                 Authorization: `Bearer ${token}`
             }
         })
-        .then(response => {
-            const fetched = response.data;
-            setclientData(fetched)
-        })
-        .catch(error => {
-            console.error("There was a problem with the request:", error);
-        });
+            .then(response => {
+                const fetched = response.data;
+                setclientData(fetched)
+            })
+            .catch(error => {
+                console.error("There was a problem with the request:", error);
+            });
     }, []);
 
+    console.log(clientData);
+
     return (
-        <div className="flex flex-row place-content-center place-items-center w-9/12 h-[500px] bg-slate-600 text-black text-center">
-            <div className="w-2/4 flex flex-col">
-                <button onClick={() => changeTab('personalData')}>Datos personales</button>
-                <button onClick={() => changeTab('personalAdress')}>Dirección de Envío</button>
-                <button onClick={() => changeTab('shopHistory')}>Historial de Compras</button>
+        <div className="flex flex-row place-content-center place-items-center w-full bg-wihte text-black text-center p-5 justify-around ">
+            <div className="w-[35%] flex flex-col justify-center bg-gray-900 h-[20rem] gap-5 text-white p-3 rounded-xl">
+                <button className='border border-gray-500 py-3 rounded-xl hover:bg-gray-700' onClick={() => changeTab('personalData')}>Datos personales</button>
+                <button className='border border-gray-500 py-3 rounded-xl hover:bg-gray-700' onClick={() => changeTab('personalAdress')}>Dirección de Envío</button>
+                <button className='border border-gray-500 py-3 rounded-xl hover:bg-gray-700' onClick={() => changeTab('shopHistory')}>Historial de Compras</button>
             </div>
-            <div className="w-2/4 flex flex-col">
+            <div className="w-2/4 flex flex-col bg-slate-600 h-[40rem] justify-center rounded-xl">
                 {clientData ? (
                     <div>
                         {clientTab === 'personalData' && (
-                            <div>
-                                <h2>Datos del Cliente</h2>
+                            <div className='flex flex-col gap-5 p-5'>
+                                <h2 className='text-white text-3xl'><strong>Datos del Cliente</strong></h2>
                                 {editando ? (
                                     <div className='flex flex-col'>
                                         <label>
@@ -92,24 +94,102 @@ const ClientInfo = () => {
                                         <button onClick={handleGuardar}>Guardar</button>
                                     </div>
                                 ) : (
-                                    <div>
-                                        <p><strong>Nombre:</strong> {clientData.name}</p>
-                                        <p><strong>Apellido:</strong> {clientData.lastName}</p>
-                                        <p><strong>Correo electrónico:</strong> {clientData.email}</p>
-                                        <p><strong>Teléfono:</strong> {clientData.phone}</p>
-                                        <p><strong>Adress:</strong> {clientData.adress}</p>
-                                        <button onClick={() => { setDatosEditados(clientData); setEditando(true); }}>Editar</button>
-                                    </div>
+                                    <>
+                                        <div className='text-gray-300 flex flex-col gap-3 items-center'>
+                                            <div className='flex w-full justify-between text-start'>
+                                                <p className='border py-2 px-5 w-[49%]' ><strong>Nombre:</strong> {clientData.name}</p>
+                                                <p className='border py-2 px-5 w-[49%]'><strong>Apellido:</strong> {clientData.lastName}</p>
+                                            </div>
+                                            <div className='flex w-full justify-between text-start'>
+                                                <p className='border py-2 px-5 w-[49%]'><strong>Teléfono:</strong> {clientData.phone}</p>
+                                                <p className=' border py-2 px-5 w-[49%]'><strong>Adress:</strong> {clientData.adress}</p>
+                                            </div>
+                                            <div className='flex w-[90%] text-start'>
+                                                <p className='border py-2 px-5 w-full'><strong>Correo electrónico:</strong> {clientData.email}</p>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <button className='border bg-gray-900 border-gray-900 text-white px-4 py-3 rounded-xl hover:bg-gray-700' onClick={() => { setDatosEditados(clientData); setEditando(true); }}>Editar</button>
+                                        </div>
+                                    </>
+
                                 )}
                             </div>
                         )}
-                    {clientTab === 'personalAdress' && (
-                    <div>
-                        <h2>Dirección de Envío</h2>
-                        <p><strong>Adress:</strong> {clientData.adress}</p>
+                        {clientTab === 'personalAdress' && (
+                            <div>
+                                <h2>Dirección de Envío</h2>
+                                <p><strong>Adress:</strong> {clientData.adress}</p>
+                            </div>
+                        )}
+                        {clientTab === 'shopHistory' && (
+                            <div>
+                                <h2>Mis Compras</h2>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Amount</th>
+                                            <th>Description</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>500</td>
+                                            <td>Oka</td>
+                                            <td>18-03-1997</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
                     </div>
-                )}
-                    {clientTab === 'shopHistory' && (
+                ) : (<p>Error fetching data</p>)}
+            </div>
+        </div>
+    );
+    /*
+                <div className="w-2/4 flex flex-col">
+                    {clientInfo === 'personalData' && (
+                        <div>
+                            <h2>Datos del Cliente</h2>
+                            {editando ? (
+                                <div className='flex flex-col'>
+                                    <label>
+                                        Nombre:
+                                        <input type="text" name="nombre" value={datosEditados.name} onChange={handleInputChange} />
+                                    </label>
+                                    <label>
+                                        Apellido:
+                                        <input type="text" name="apellido" value={datosEditados.lastName} onChange={handleInputChange} />
+                                    </label>
+                                    <label>
+                                        Correo electrónico:
+                                        <input type="email" name="email" value={datosEditados.email} onChange={handleInputChange} />
+                                    </label>
+                                    <label>
+                                        Teléfono:
+                                        <input type="tel" name="telefono" value={datosEditados.phone} onChange={handleInputChange} />
+                                    </label>
+                                    <button onClick={handleGuardar}>Guardar</button>
+                                </div>
+                            ) : (
+                                <div>
+                                    <p><strong>Nombre:</strong> {clientData.name}</p>
+                                    <p><strong>Apellido:</strong> {clientData.lastName}</p>
+                                    <p><strong>Correo electrónico:</strong> {clientData.email}</p>
+                                    <p><strong>Teléfono:</strong> {clientData.phone}</p>
+                                    <button onClick={() => { setDatosEditados(clientData); setEditando(true); }}>Editar</button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                    {clientInfo === 'personalAdress' && (
+                        <div>
+                            <h2>Dirección de Envío</h2>
+                        </div>
+                    )}
+                    {clientInfo === 'shopHistory' && (
                         <div>
                             <h2>Mis Compras</h2>
                             <table>
@@ -130,75 +210,8 @@ const ClientInfo = () => {
                             </table>
                         </div>
                     )}
-                    </div>
-                ) : (<p>Error fetching data</p>)}
-            </div>
-        </div>
-    );
-/*
-            <div className="w-2/4 flex flex-col">
-                {clientInfo === 'personalData' && (
-                    <div>
-                        <h2>Datos del Cliente</h2>
-                        {editando ? (
-                            <div className='flex flex-col'>
-                                <label>
-                                    Nombre:
-                                    <input type="text" name="nombre" value={datosEditados.name} onChange={handleInputChange} />
-                                </label>
-                                <label>
-                                    Apellido:
-                                    <input type="text" name="apellido" value={datosEditados.lastName} onChange={handleInputChange} />
-                                </label>
-                                <label>
-                                    Correo electrónico:
-                                    <input type="email" name="email" value={datosEditados.email} onChange={handleInputChange} />
-                                </label>
-                                <label>
-                                    Teléfono:
-                                    <input type="tel" name="telefono" value={datosEditados.phone} onChange={handleInputChange} />
-                                </label>
-                                <button onClick={handleGuardar}>Guardar</button>
-                            </div>
-                        ) : (
-                            <div>
-                                <p><strong>Nombre:</strong> {clientData.name}</p>
-                                <p><strong>Apellido:</strong> {clientData.lastName}</p>
-                                <p><strong>Correo electrónico:</strong> {clientData.email}</p>
-                                <p><strong>Teléfono:</strong> {clientData.phone}</p>
-                                <button onClick={() => { setDatosEditados(clientData); setEditando(true); }}>Editar</button>
-                            </div>
-                        )}
-                    </div>
-                )}
-                {clientInfo === 'personalAdress' && (
-                    <div>
-                        <h2>Dirección de Envío</h2>
-                    </div>
-                )}
-                {clientInfo === 'shopHistory' && (
-                    <div>
-                        <h2>Mis Compras</h2>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Amount</th>
-                                    <th>Description</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>500</td>
-                                    <td>Oka</td>
-                                    <td>18-03-1997</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-            </div>):(<div>Error fetching data</div>)}
-    );*/
+                </div>):(<div>Error fetching data</div>)}
+        );*/
 };
 
 export default ClientInfo;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
-
-const productos = [
-    { id: 1, title: 'producto 1', category: 'Electrónicos', brand: 'Marca A', price: 76, discount: 10 },
-    { id: 2, title: 'producto 2', category: 'Ropa', brand: 'Marca B', price: 50, discount: 0 },
-    { id: 3, title: 'producto 3', category: 'Electrónicos', brand: 'Marca A', price: 200, discount: 20 },
-    // Agrega más productos según sea necesario
-];
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 // Title search filter component
 function TitleFilter({ onTitleChange }) {
     return (
-        <div class="relative">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        <div className="relative">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
             </div>
-            <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-900 dark:border-gray-900 dark:placeholder-gray-200 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Buscar por título" onChange={(e) => onTitleChange(e.target.value)}></input>
+            <input type="text" id="voice-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-900 dark:border-gray-900 dark:placeholder-gray-200 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Buscar por título" onChange={(e) => onTitleChange(e.target.value)}></input>
         </div>
     );
 }
@@ -24,7 +18,7 @@ function CategoryFilter({ categories, onCategoryChange }) {
     return (
         <select
             onChange={(e) => onCategoryChange(e.target.value)}
-            className="border px-2 py-1 rounded-md bg-gray-900 text-gray-200 border-gray-900 w-full"
+            className="border px-2 py-3 rounded-md bg-gray-900 text-gray-200 border-gray-700 w-full"
         >
             <option value="">Todas las categorías</option>
             {categories.map(category => (
@@ -39,7 +33,7 @@ function BrandFilter({ brands, onBrandChange }) {
     return (
         <select
             onChange={(e) => onBrandChange(e.target.value)}
-            className="border bg-gray-900 text-gray-200 border-gray-900 px-2 py-1 rounded-md w-full"
+            className="border bg-gray-900 text-gray-200 border-gray-700 px-2 py-3 rounded-md w-full"
         >
             <option value="">Todas las marcas</option>
             {brands.map(brand => (
@@ -52,20 +46,22 @@ function BrandFilter({ brands, onBrandChange }) {
 // Filter component by price range
 function PriceFilter({ minPrice, maxPrice, onMinPriceChange, onMaxPriceChange }) {
     return (
-        <div className='flex flex-wrap gap-2'>
+        <div className='flex flex-wrap gap-1'>
             <input
                 type="number"
                 placeholder="Precio mínimo"
                 value={minPrice}
+                min={0}
                 onChange={(e) => onMinPriceChange(e.target.value)}
-                className="border bg-gray-900 text-gray-200 border-gray-900 px-2 py-1 rounded-md w-full"
+                className="border bg-gray-900 text-gray-200 border-gray-700 px-2 py-1 rounded-md w-full"
             />
             <input
                 type="number"
                 placeholder="Precio máximo"
                 value={maxPrice}
+                min={0}
                 onChange={(e) => onMaxPriceChange(e.target.value)}
-                className="border bg-gray-900 text-gray-200 border-gray-900 px-2 py-1 rounded-md w-full"
+                className="border bg-gray-900 text-gray-200 border-gray-700 px-2 py-1 rounded-md w-full"
             />
         </div>
     );
@@ -74,22 +70,23 @@ function PriceFilter({ minPrice, maxPrice, onMinPriceChange, onMaxPriceChange })
 // Filter component by promotions (% discount)
 function DiscountFilter({ onDiscountChange }) {
     return (
-      <select
-        onChange={(e) => onDiscountChange(e.target.value)}
-        className="border border-gray-900 bg-gray-900 text-gray-200 px-2 py-1 rounded-md w-full"
-      >
-        <option value="">Todos los descuentos</option>
-        <option value="0-10">0% - 10%</option>
-        <option value="11-20">11% - 20%</option>
-        <option value="21-30">21% - 30%</option>
-        <option value="31-40">31% - 40%</option>
-        <option value="41-50">41% - 50%</option>
-        <option value="51-99">+51%</option>
-      </select>
+        <select
+            onChange={(e) => onDiscountChange(e.target.value)}
+            className="border border-gray-700 bg-gray-900 text-gray-200 px-2 py-3 rounded-md w-full "
+        >
+            <option value="">Todos los descuentos</option>
+            <option value="0-10">0% - 10%</option>
+            <option value="11-20">11% - 20%</option>
+            <option value="21-30">21% - 30%</option>
+            <option value="31-40">31% - 40%</option>
+            <option value="41-50">41% - 50%</option>
+            <option value="51-99">+51%</option>
+        </select>
     );
-  }
+}
 
 function Shop() {
+    const [products, setProducts] = useState([]);
     const [titleFilter, setTitleFilter] = useState('');
     const [categoryFilter, setCategoryFilter] = useState('');
     const [brandFilter, setBrandFilter] = useState('');
@@ -112,35 +109,49 @@ function Shop() {
         setMinPriceFilter('');
         setMaxPriceFilter('');
         setDiscountFilter('');
-      };
+    };
+
+    useEffect(() => {
+        axios.get('api/products/all')
+            .then(res => {
+                setProducts(res.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
 
     // Logic for filtering products based on selected filters
-    const filteredProducts = productos.filter(producto => {
+    const filteredProducts = products.filter(product => {
         // Lógica para cada filtro aquí
-        const isInTitle = producto.title.toLowerCase().includes(titleFilter.toLowerCase());
-        const isInCategory = categoryFilter === '' || producto.category === categoryFilter;
-        const isInBrand = brandFilter === '' || producto.brand === brandFilter;
-        const isWithinPriceRange = (minPriceFilter === '' || parseFloat(producto.price) >= parseFloat(minPriceFilter))
-          && (maxPriceFilter === '' || parseFloat(producto.price) <= parseFloat(maxPriceFilter));
-    
+        const isInTitle = product.name.toLowerCase().includes(titleFilter.toLowerCase());
+        const isInCategory = categoryFilter === '' || product.category === categoryFilter;
+        const isInBrand = brandFilter === '' || product.brand === brandFilter;
+        const isWithinPriceRange = (minPriceFilter === '' || parseFloat(product.finalPrice) >= parseFloat(minPriceFilter))
+            && (maxPriceFilter === '' || parseFloat(product.FinalPrice) <= parseFloat(maxPriceFilter));
+
         let isWithinDiscountRange = true;
         if (discountFilter !== '') {
-          const [minDiscount, maxDiscount] = discountFilter.split('-');
-          const discountPercentage = producto.discount;
-          isWithinDiscountRange = parseFloat(discountPercentage) >= parseFloat(minDiscount) 
-            && parseFloat(discountPercentage) <= parseFloat(maxDiscount);
+            const [minDiscount, maxDiscount] = discountFilter.split('-');
+            const discountPercentage = product.promos;
+            isWithinDiscountRange = parseFloat(discountPercentage) >= parseFloat(minDiscount)
+                && parseFloat(discountPercentage) <= parseFloat(maxDiscount);
         }
-    
+
         return isInTitle && isInCategory && isInBrand && isWithinPriceRange && isWithinDiscountRange;
-      });
+    });
+
+    console.log(filteredProducts);
 
     // Obtain unique product categories and brands
-    const categories = Array.from(new Set(productos.map(producto => producto.category)));
-    const brands = Array.from(new Set(productos.map(producto => producto.brand)));
+    const categories = Array.from(new Set(products.map(product => product.category)));
+    const brands = Array.from(new Set(products.map(product => product.brand)));
 
     return (
-        <div className='flex pr-10 pl-3 py-5 gap-5 flex-wrap'>
-            <div className="flex flex-col gap-4 w-[25%] border-[3px] border-gray-900 p-1 rounded-xl">
+        <div className='flex pr-10 pl-3 py-5 gap-5 flex-wrap min-h-3/5'>
+            <div className="flex flex-col gap-4 w-[25%] h-[40rem] border-[3px] bg-gray-900 p-1 rounded-xl border-gray-900 justify-center md:sticky md:top-5">
+                <div className="flex justify-center">
+                    <h1 className="font-bold text-white text-2xl" >Filtros</h1>
+                </div>
                 <div>
                     <CategoryFilter className categories={categories} onCategoryChange={handleCategoryChange} />
                 </div>
@@ -158,8 +169,8 @@ function Shop() {
                 <div>
                     <DiscountFilter onDiscountChange={handleDiscountChange} />
                 </div>
-                <div>
-                    <button className="border border-gray-900 bg-gray-900 text-gray-200 px-2 py-1 rounded-md w-full" onClick={clearFilters}>Limpiar filtros</button>
+                <div className='flex justify-center gap-3'>
+                    <button className="border border-gray-700 bg-gray-900 text-gray-200 px-4 py-3 rounded-md" onClick={clearFilters}>Limpiar filtros</button>
                 </div>
             </div>
             <div className='flex flex-col items-center w-[70%]'>
@@ -170,11 +181,19 @@ function Shop() {
                 </div>
                 <div className='w-full'>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
-                        {filteredProducts.map(producto => (
-                            <div key={producto.id} className="border border-gray-300 p-4 rounded-md">
-                                <h2 className="text-lg font-bold">{producto.title}</h2>
-                                <p>{producto.description}</p>
-                                <p>${producto.price}</p>
+                        {filteredProducts.map(product => (
+                            <div key={product.id} className="bg-white rounded-lg shadow-lg p-4">
+                                <img src={product.image} alt={product.name} className="w-full h-48 object-cover mb-4 rounded-lg" />
+                                <h2 className="text-xl font-bold">{product.name}</h2>
+                                <p className="text-gray-600">{product.description}</p>
+                                <div className="mt-4 flex flex-wrap justify-between items-center">
+                                    <span className="text-gray-800 font-bold">{product.finalPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                                    <a href="/productdetails">
+                                        <button className="ml-2 bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
+                                            Ver Detalle
+                                        </button>
+                                    </a>
+                                </div>
                             </div>
                         ))}
                     </div>
