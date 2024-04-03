cac542d9b440ad450ae4e8458a93fabe1db600e5
Dure Matias Miguel <72953094+MatiaSalvatore@users.noreply.github.com>
2024-04-02 21:40:47 -0300

    Cart function almost done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useCart } from '../utils/cart';
+import axios from 'axios';
 
 function Cart() {
   const shippingPrice = 100; 
   const { cart, removeFromCart } = useCart();
   const [quantities, setQuantities] = useState(Array(cart.length).fill(1));
-
+  const [cartDetail, setCartDetail] = useState()
 
   const calculateTotalPrice = () => {
     let totalPrice = 0;
@@ -19,9 +20,42 @@ function Cart() {
     const newQuantities = [...quantities];
     newQuantities[index] = parseInt(event.target.value, 10); 
     setQuantities(newQuantities);
+    const cartarray = [];
+  
+      cart.forEach((product, index) => {
+        cartarray.push({
+          productoId: product.productoDTOID,
+          amount: product.finalPrice * (1 - product.promos / 100),
+          quantity: quantities[index]
+        });
+      });
+    /*const cartarray = cart.map(product => {
+      return {
+        productoID: product.productoDTOID,
+        amount: product.finalPrice * (1-product.promos/100),
+        quantity: quantities[index]
+      }
+    })*/
+    setCartDetail(cartarray)
+    console.log(cartarray)
   };
 
+  const handleOnPay =async (e)=>{
+    const token = localStorage.getItem("token")
+    e.preventDefault()
+    axios.post("/api/carts/cartOnline",cartDetail, {
+      headers: {
+        Authorization: `Bearer ${token}`
+    }
+    }) .then((res) => {
+      alert('Purchase complete!')
+    })
+    .catch((err) => {
+        console.log(err);
+        alert('Theres an error in the purchase');
+    });
 
+    }
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ marginRight: '20px' }}>
@@ -59,7 +93,7 @@ function Cart() {
         </div>
       </div>
       <div>
-        <button style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
+        <button onClick={handleOnPay} style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
       </div>
     </div>
   );
