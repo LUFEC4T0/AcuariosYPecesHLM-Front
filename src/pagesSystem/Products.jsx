804de6540e239ed35e1974579b226872ecd05b41
OrlandoConci <orlandomarceloconci@gmail.com>
2024-04-03 20:25:13 -0300

    Navegación marcada en system

--- src/pagesSystem/Products.jsx
+++ src/pagesSystem/Products.jsx
@@ -8,9 +8,18 @@ const Product = () => {
     const [visual1, setVisual1] = useState(true);
     const [visual2, setVisual2] = useState(false);
 
-    function handleclick() {
-      setVisual1(!visual1);
-      setVisual2(!visual2);
+    function handleclick1(e) {
+        setVisual1(true)
+        setVisual2(false)
+        console.log(`visual 1: ${!visual1}`)
+        console.log(`visual 2: ${visual2}`)
+    }
+
+    function handleclick2(e) {
+        setVisual1(false)
+        setVisual2(true)
+        console.log(`visual 1: ${visual1}`)
+        console.log(`visual 2: ${!visual2}`)
     }
 
     const [newProduct, setNewProduct] = useState({
@@ -41,18 +50,21 @@ const Product = () => {
     },[])
      
     console.log(products)
+    const color1 = "border border-white border-double font-bold text-center mt-11 mx-2 h-11 w-full text-white max-w-[24rem] bg-gray-900"
+    const color2 = "font-bold text-center mt-11 mx-2 h-11 w-full text-black max-w-[24rem] bg-gray-300"
 
     return (
         <>
         
-        <main className="flex flex-col justify-center items-center m-5 gap-6 ">
-            <div>
-                <button onClick={handleclick} className="font-bold text-center mt-11 mx-2 bg-gray-300 h-11 border-2 w-40 max-w-[30rem]">Ver Productos</button>
-                <button onClick={handleclick} className="font-bold text-center mt-11 mx-2 bg-gray-300 h-11 border-2 w-40 max-w-[30rem]">Agregar Producto</button>
+        <main className="flex flex-col justify-center items-center m-5">
+            <h1 className=" text-white text-2xl  text-center">Lista de Productos</h1>
+            <div className="flex w-full justify-center mb-5 gap-3">
+                <button onClick={handleclick1} className={visual1 ? color1 : color2}>Ver Productos</button>
+                <button onClick={handleclick2} className={visual2 ? color1 : color2}>Agregar Producto</button>
             </div>
-            {visual1 && <div>
-            <h1 className=" text-white text-2xl  text-center mb-5">Lista de Productos</h1>
             <div className="border-t border-2 border-white w-[50rem] self-center mb-5"></div>
+            {visual1 && <div>
+            
             <div className="flex gap-2 text-white border-2 border-solid border-white w-[500px] p-4">
                 <p className="w-44 text-center">Artículo</p>
                 <p className="w-32 text-right">Detalle</p>
