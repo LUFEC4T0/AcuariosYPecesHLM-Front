ec68b532e669d29954b2752efde68744f6c3a768
Nico J <niicoo995@gmail.com>
2024-04-03 20:28:44 -0300

    pruebas sales

--- src/pagesSystem/Sales.jsx
+++ src/pagesSystem/Sales.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState }  from "react";
+import { useStore } from "react-redux";
 
 
 
 const Sales = () => {
+
+    //Para ventas
 
     const [sales, setSales] = useState([])
 
@@ -24,6 +27,72 @@ const Sales = () => {
 
     })
 
+
+    const [newSale, setNewSale] = useState({
+        details: '',
+        finalAmount: 0.0,
+        paidMethod: [],
+        taxes: [],
+        employeeId: 1,
+        cartId: 0,
+        })
+
+    const handleSubmit = async  (e) => {
+        e.preventDeafult()
+
+
+        axios.post("api/sales/", newSale, {
+            headers: {
+                Authorization: `Bearer ${token}`
+              }
+        }).then((res)=> {
+            console.log(res.data)
+        }).catch((err)=> {
+            console.log(err)
+        })
+    }
+
+    const handleInput = async (e) => {
+        setNewSale({ ...newSale, [e.target.name]: e.target.value });
+        console.log(newSale)
+      };
+
+
+
+      //Para carrito
+
+      //RecordCartDetailStoreDTO
+      const [newCartDetail, setNewCartDetail] = useState({
+        quantity: 0,
+        amount: 0,
+        productId: 0,
+        rut: ''
+      })
+
+
+
+      const listaDetalles = []
+
+      //Cuando llame a cartController llame a salesController
+
+
+      //Traer listado de clientes
+
+      const [clientsList, setClientsList] = useStore([])
+
+      axios.get('api/clients/clientStore' ,{
+        headers: {
+            Authorization: `Bearer ${token}`,
+          }
+      }).then((res) => {
+        setClientsList(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+      
+
     return(
         <>
             {sales.map((sale)=> {
@@ -31,6 +100,16 @@ const Sales = () => {
                     <p>{sale.finalAmount}</p>
                 </div>
             })}
+
+            <form action={handleSubmit}>
+                <h3>Registrar Venta</h3>
+                <input type="text" onChange={handleInput}/>
+                <input type="text" onChange={handleInput}/>
+                <input type="text" onChange={handleInput}/>
+                <input type="text" onChange={handleInput}/>
+                <input type="text" />
+                <input type="text" />
+            </form>
 
         </>
     )
