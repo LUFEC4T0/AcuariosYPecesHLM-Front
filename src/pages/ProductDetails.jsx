e362b3e10a0feaf8b38c6ea95e6acbefe923d8bc
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-04-03 19:26:43 -0300

    Arreglo para product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -53,11 +53,18 @@ const ProductDetails = () => {
                     </h1>
                 </div>
                 <div className="border-b border-gray-200 pb-6">
-                    <h2 className="lg:text-base text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2 line-through	">{numberFormatter.format(productState.finalPrice)}</h2>
-                    <section className="flex flex-row">
-                        <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2">{numberFormatter.format(productState.finalPrice*(1-productState.promos/100))}</h1>
-                        <h1 className="lg:text-1xl text-xl lg:leading-6 leading-7 text-green-800 mt-2 ml-4">{productState.promos}% OFF</h1>
-                    </section>
+                    {productState.promos !== 0 && (<>
+                     <h2 className="lg:text-base text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2 line-through	">{numberFormatter.format(productState.finalPrice)}</h2>
+                     <section className="flex flex-row">
+                         <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2">{numberFormatter.format(productState.finalPrice*(1-productState.promos/100))}</h1>
+                         <h1 className="lg:text-1xl text-xl lg:leading-6 leading-7 text-green-800 mt-2 ml-4">{productState.promos}% OFF</h1>
+                     </section>                       
+                     </>
+                    )}
+                    {productState.promos === 0 && (<>
+                     <h2 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2">{numberFormatter.format(productState.finalPrice)}</h2>          
+                     </>
+                    )}                    
                 </div>
                 <div className="py-4 border-b border-gray-200 flex items-center justify-between">
                     <p className="text-base leading-4 text-gray-800">Marca</p>
@@ -68,21 +75,22 @@ const ProductDetails = () => {
                         </svg>
                     </div>
                 </div>
-                <div className="py-4 border-b border-gray-200 flex items-center justify-between">
-                    <p className="text-base leading-4 text-gray-800">Capacidad</p>
-                    <div className="flex items-center justify-center">
-                        <p className="text-sm leading-none text-gray-600 px-5">{productState.details}</p>
-                        <svg className="cursor-pointer" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L5 5L1 9" stroke="#4B5563" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
+                    <div className="py-4 border-b border-gray-200 flex items-center justify-between">
+                        <p className="text-base leading-4 text-gray-800">Detalles</p>
+                        <div className="flex items-center justify-center">
+                            <p className="text-sm leading-none text-gray-600 px-5">{productState.details}</p>
+                            <svg className="cursor-pointer" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 1L5 5L1 9" stroke="#4B5563" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </div>
                     </div>
-                </div>
                 <div className="py-4 border-b border-gray-200 flex items-center justify-between">
                 </div>
                 <button
                     className="
 						focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800
 						text-base
+                        rounded-lg
 						flex
 						items-center
 						justify-center
@@ -92,6 +100,7 @@ const ProductDetails = () => {
 						w-full
 						py-4
 						hover:bg-gray-700
+                        transform active:scale-95 transition-transform
 					"
                     onClick={handleAddToCart}
                 >
