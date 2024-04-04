93a49f9abb5f626e74be210aebdf6d93b4e50897
Juan Jose Faella <faellajuan19@gmail.com>
2024-04-04 01:15:34 -0300

    Se hizo el carrito

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -31,13 +31,6 @@ function Cart() {
           quantity: quantities[index]
         });
       });
-    /*const cartarray = cart.map(product => {
-      return {
-        productoID: product.productoDTOID,
-        amount: product.finalPrice * (1-product.promos/100),
-        quantity: quantities[index]
-      }
-    })*/
     setCartDetail(cartarray)
     console.log(cartarray)
   };
@@ -72,14 +65,15 @@ function Cart() {
     <div className=''>
       <div className='flex justify-around items-center'>
         <div className='flex flex-col items-center p-10 gap-10'>
-        <h2>Shopping Cart</h2>
-        <div className=''>
-        <ul className=''>
+        <h2 className='text-2xl font-extrabold'>Shopping Cart:</h2>
+        <div>
+        <ul>
         {cart.map((item, index) => (
         <li key={index} className='flex justify-between items-center gap-10 border-r p-5 border-black'>
           <div>
             <img className="w-36" src={item.image} alt="" />
           </div>
+          <div className='flex flex-col gap-3'>
           <div className='flex gap-5 justify-between'>
             <p className='font-bold'>
               Producto: 
@@ -90,15 +84,20 @@ function Cart() {
             <p className='font-bold'>
               Precio:
             </p>
-            {item.finalPrice}
+            {(item.finalPrice).toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}
           </div>
           <div className='flex gap-5 justify-between'>
             <p className='font-bold'>Descuento:</p>
-            {item.finalPrice * (item.promos/100)}
+            {(item.finalPrice * (item.promos/100)).toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}
           </div>
           <div className='flex gap-5 justify-between'>
             <p className='font-bold'>Cantidad:</p>
+            <p className='opacity-60'>
+            (Seleccione)
+          </p>
             <input 
+            
+            className='text-right border border-black rounded-lg'
             type="number" 
             step="1" 
             min="1" 
@@ -106,54 +105,48 @@ function Cart() {
             value={quantities[index]} 
             onChange={(event) => handleQuantityChange(index, event)}
           />
+          
           </div>
-          <p>
-            {"Total: "+ item.finalPrice * (1-item.promos/100) * quantities[index]} 
-          </p>
-
-      <button onClick={() => removeFromCart(index)}>Remove</button>
+          <div className='flex gap-5 justify-between'>
+            <p className='font-bold'>
+            Total: 
+            </p>
+            {(item.finalPrice * (1-item.promos/100) * quantities[index]).toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}
+          </div>
+          
+          </div>
+      <button className="bg-red-500 p-2 rounded-lg text-white hover:scale-95" onClick={() => removeFromCart(index)}>X Remove</button>
     </li>
   ))}
 </ul>
         </div>
-        <div>
-          <strong>Subtotal:</strong> {calculateTotalPrice()}
-          <br />
-          <strong>Shipping Price:</strong> ${shippingPrice}
-          <br />
-          <strong>Taxes:</strong> ${calculateTotalPrice()* 0.21} 
-          <br />
-          <strong>Total:</strong> ${calculateTotalPrice()* 0.21+ calculateTotalPrice()+shippingPrice}
-        </div>
       </div>
-      <div>
-        <button  style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
-        <label>Payment Method: </label>
-        <select>
-          <option value="DEBIT" selected>DEBIT</option>
-          <option value= "CREDIT">CREDIT</option>
-        </select>
-    
-      </div>
-        <div className='flex flex-col justify-center items-start gap-5'>
-          <div className='flex gap-5'>
-            <p className='font-bold'>Subtotal:</p> 
-            {calculateTotalPrice()}
+        <div className='flex sticky top-[6.3rem] mb-5 flex-col rounded-lg justify-center gap-5 bg-gray-900 p-10 py-20 text-white '>
+          <div className='flex gap-5 justify-between'>
+            <p className='font-bold'>Sub Total:</p> 
+            {(calculateTotalPrice()).toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}
           </div>
-          <div className='flex gap-5'>
-            <p className='font-bold'>Shipping Price:</p> 
-            ${shippingPrice}
+          <div className='flex gap-5 justify-between'>
+            <p className='font-bold'>Envio:</p> 
+            {shippingPrice.toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}
           </div>
-          <div className='flex gap-5'>
-            <p className='font-bold'>Taxes:</p>
-            ${calculateTotalPrice()* 0.21} 
+          <div className='flex gap-5 justify-between'>
+            <p className='font-bold'>Impuestos:</p>
+            {(calculateTotalPrice()* 0.21).toLocaleString( 'en-US', { style:'currency', currency:'USD' } )} 
           </div>
-          <div className='flex gap-5'>
+          <div className='flex gap-5 justify-between'>
             <p className='font-bold'>Total:</p>
-            ${calculateTotalPrice()* 0.21+ calculateTotalPrice()+shippingPrice}
+            {(calculateTotalPrice()* 0.21+ calculateTotalPrice()+shippingPrice).toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}
           </div>
+          <div className='flex gap-5 justify-between'>
+          <label className='font-bold'>Metodo de pago: </label>
+          <select className='bg-gray-900'>
+            <option value="DEBIT" selected>DEBIT</option>
+            <option value= "CREDIT">CREDIT</option>
+          </select>
+        </div>
           <div>
-           <button onClick={handleOnPay} style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
+           <button className='bg-green-600 w-[100%] rounded-lg font-bold hover:scale-95 ' onClick={handleOnPay} style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
           </div>
         </div>
         </div>
