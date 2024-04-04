f8808ae6e74521efd6c825ad3ea0df82749195ea
Juan Jose Faella <faellajuan19@gmail.com>
2024-04-03 22:56:03 -0300

    juan

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import { useCart } from '../utils/cart';
-import axios from 'axios';
+
 
 function Cart() {
   const shippingPrice = 100; 
   const { cart, removeFromCart } = useCart();
   const [quantities, setQuantities] = useState(Array(cart.length).fill(1));
-  const [cartDetail, setCartDetail] = useState()
-  const [purchaseData,setPurchaseData] = useState({details:'',finalAmount:'',paidMethod:'',taxes:'',cartId:''})
-  
+
+
   const calculateTotalPrice = () => {
     let totalPrice = 0;
     cart.forEach((item, index) => {
-      totalPrice += item.finalPrice * (1-item.promos/100) * quantities[index];
+      totalPrice += item.finalPrice * quantities[index];
     });
     return totalPrice;
   };
@@ -21,71 +20,42 @@ function Cart() {
     const newQuantities = [...quantities];
     newQuantities[index] = parseInt(event.target.value, 10); 
     setQuantities(newQuantities);
-    const cartarray = [];
-  
-      cart.forEach((product, index) => {
-        cartarray.push({
-          productoId: product.productoDTOID,
-          amount: product.finalPrice * (1 - product.promos / 100),
-          quantity: quantities[index]
-        });
-      });
-    /*const cartarray = cart.map(product => {
-      return {
-        productoID: product.productoDTOID,
-        amount: product.finalPrice * (1-product.promos/100),
-        quantity: quantities[index]
-      }
-    })*/
-    setCartDetail(cartarray)
-    console.log(cartarray)
   };
 
-  const handleOnPay =async (e)=>{
-    const token = localStorage.getItem("token")
-    e.preventDefault()
-    axios.post("/api/carts/cartOnline",cartDetail, {
-      headers: {
-        Authorization: `Bearer ${token}`
-    }
-    }) .then((res) => {
-      alert('Purchase complete!')
-      axios.post("")
-    })
-    .catch((err) => {
-        console.log(err);
-        alert('Theres an error in the purchase');
-    });
 
-    }
   return (
     <div className=''>
-      <div className='flex justify-around'>
-        <div className='flex flex-col'>
+      <div className='flex justify-around items-center'>
+        <div className='flex flex-col items-center p-10 gap-10'>
         <h2>Shopping Cart</h2>
         <div className=''>
         <ul className=''>
         {cart.map((item, index) => (
-        <li key={index} className='flex flex-col justify-center items-start gap-5'>
-          <div className='flex gap-5'>
+        <li key={index} className='flex justify-between items-center gap-10 border-r p-5 border-black'>
+          <div>
+            <img className="w-36" src={item.image} alt="" />
+          </div>
+          <div className=' '>
+          <div className='flex gap-5 justify-between'>
             <p className='font-bold'>
               Producto: 
             </p>
             {item.name}
           </div>
-          <div className='flex gap-5'>
+          <div className='flex gap-5 justify-between'>
             <p className='font-bold'>
               Precio:
             </p>
             {item.finalPrice}
           </div>
-          <div className='flex gap-5'>
+          <div className='flex gap-5 justify-between'>
             <p className='font-bold'>Descuento:</p>
             {item.finalPrice * (item.promos/100)}
           </div>
-          <div className='flex gap-5'>
+          <div className='flex gap-5 justify-between'>
             <p className='font-bold'>Cantidad:</p>
             <input 
+             className='text-right '
                 type="number" 
                 step="1" 
                 min="1" 
@@ -94,42 +64,24 @@ function Cart() {
                 onChange={(event) => handleQuantityChange(index, event)}
               />
           </div>
-          <p>
-            {"Total: "+ item.finalPrice * (1-item.promos/100) * quantities[index]} 
-          </p>
-       
-
-<<<<<<< HEAD
-        <button onClick={() => removeFromCart(index)}>Remove</button>
-      </li>
+          </div>
+        <button className='px-5 bg-red-500 text-white rounded-lg hover:text-blue-600' onClick={() => removeFromCart(index)}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+          Remove</button>
+          <div className='flex gap-5 justify-between'>
+            <p className='font-bold'>
+            Total: 
+            </p>
+            {item.finalPrice * (1-item.promos/100) * quantities[index]} 
+          </div>
+        </li>
+      
       ))}
+      
     </ul>
 </div>
-=======
-      <button onClick={() => removeFromCart(index)}>Remove</button>
-    </li>
-  ))}
-</ul>
-        </div>
-        <div>
-          <strong>Subtotal:</strong> {calculateTotalPrice()}
-          <br />
-          <strong>Shipping Price:</strong> ${shippingPrice}
-          <br />
-          <strong>Taxes:</strong> ${calculateTotalPrice()* 0.21} 
-          <br />
-          <strong>Total:</strong> ${calculateTotalPrice()* 0.21+ calculateTotalPrice()+shippingPrice}
-        </div>
-      </div>
-      <div>
-        <button onClick={handleOnPay} style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
-        <label>Payment Method: </label>
-        <select>
-          <option value="DEBIT" selected>DEBIT</option>
-          <option value= "CREDIT">CREDIT</option>
-        </select>
-      
->>>>>>> bc03eaeca7609d77edbaa38bb444999043109145
       </div>
         <div className='flex flex-col justify-center items-start gap-5'>
           <div className='flex gap-5'>
@@ -149,7 +101,7 @@ function Cart() {
             ${calculateTotalPrice()* 0.21+ calculateTotalPrice()+shippingPrice}
           </div>
           <div>
-          <button style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
+           <button style={{ padding: '10px 20px', fontSize: '16px' }}>Pay Now</button>
           </div>
         </div>
         </div>
