28ce3673426048a5d40b8c9eb307086102e4640d
LUFEC4T0 <luigi02190622@gmail.com>
2024-04-03 23:44:10 -0500

    commit final luigi

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -28,9 +28,9 @@ const ProductDetails = () => {
       };
 
     return (
-        <div className="md:flex items-start justify-center py-12 2xl:px-20 md:px-6 px-4">
-            <div className="xl:w-2/6 lg:w-2/5 w-80 md:block hidden">
-                <img src={productState.image} className="w-full h-full rounded object-cover mb-4 rounded-lg" />
+        <div className="md:flex items-center justify-center py-12 2xl:px-20 md:px-6 px-4">
+            <div className="xl:w-2/6 lg:w-2/5 w-80 md:block sm:w-full sm:flex sm:justify-center">
+                <img src={productState.image} className="w-full h-full object-cover mb-4 rounded-lg sm:w-[15rem]" />
             </div>
 
             <div className="xl:w-2/5 md:w-1/2 lg:ml-8 md:ml-6 md:mt-0 mt-6">
